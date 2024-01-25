f4e74bf0789689fddb8ebc3d5f6904d7f4ee75e4
Ayush1617 <agrawalayush1617@gmail.com>
2024-01-25 15:20:22 +0530

    hello

---
+++ article/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+
+def index(request):
+    all_articles = Article.objects.all()
+    context = {
+            'articles':all_articles
+    }
+    return render(request,'article/index.html',context)
