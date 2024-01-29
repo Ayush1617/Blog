0ea8b95bd952acda89cb206c6703fe74a648853d
Ayush1617 <agrawalayush1617@gmail.com>
2024-01-29 13:54:22 +0530

    adding folders

--- article/views.py
+++ article/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import *
 # Create your views here.
+#context processers -category
+def categories(request):
+    categories=Category.objects.all()
+
+    return{
+        'categories':categories
+    }
+
+
+
 
 def index(request):
     all_articles = Article.objects.all()
@@ -8,3 +18,12 @@ def index(request):
             'articles':all_articles
     }
     return render(request,'article/index.html',context)
+
+def single_article(request,pk):
+    article = Article.objects.get(pk=pk)
+    context={
+
+'article':article
+     }
+
+    return render(request,'article/article.html',context)
